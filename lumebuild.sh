ae0dac84f860c981e357a8c386d1ea1f09d758af
haturatu <taro@eyes4you.org>
2024-10-11 01:40:49 +0900

    wip

---
+++ lumebuild.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+LUME_DIR="/var/www/html/soulmining"
+SRC_DIR="$LUME_DIR/src"
+BUILD_DIR="site"
+WEBPSH="/your/webp/convert/path"
+
+export DENO_INSTALL="/home/$USER/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
+
+cd "$SRC_DIR" || exit
+
+git add . || git init && git add . || exit
+
+git commit -m "`date`"
+
+if [ $? -eq 0 ]; then
+  $WEBPSH
+  cd $LUME_DIR || exit
+  deno task lume --dest=$BUILD_DIR > /dev/null 2>&1
+else
+  exit 1
+fi
+
